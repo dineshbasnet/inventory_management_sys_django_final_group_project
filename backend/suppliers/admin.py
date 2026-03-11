b9ab77ae918e042284455d50a8f405bd975f9def
yadav577 <yadavbibek233@gmail.com>
2026-03-11 20:43:27 +0545

    Added  product-Supplier models, serializers, views and urls

--- backend/suppliers/admin.py
+++ backend/suppliers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Supplier
 
-# Register your models here.
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'company', 'email', 'phone', 'is_active']
+    list_filter   = ['is_active']
+    search_fields = ['name', 'company', 'email']
--- backend/suppliers/views.py
+++ backend/suppliers/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
+from .models import Supplier
+from .serializers import SupplierSerializer
+from core.permissions.roles import IsManagerOrAdmin, IsAnyStaff
 
-# Create your views here.
+
+class SupplierViewSet(viewsets.ModelViewSet):
+ 
+    queryset         = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    filter_backends  = [SearchFilter]
+    search_fields    = ['name', 'company', 'email']
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsManagerOrAdmin()]
+        return [IsAnyStaff()]
